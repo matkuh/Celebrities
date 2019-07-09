051d231404e701db52c3a21b620f0b09287c1535
matkuh <matkuh@gmail.com>
2019-07-08 23:47:06 -0700

    set up connection.js, schema and seeds.sql creating celebrity database and add 5 celebrities,set up server.js

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE celebrity_db;
+USE celebrity_db;
+
+CREATE TABLE celebrities
+(
+	id int NOT NULL AUTO_INCREMENT,
+	celebrity_name varchar(255) NOT NULL,
+	exiled BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO celebrities (celebrity_name) VALUES ('Kim Kardashian');
+INSERT INTO celebrities (celebrity_name) VALUES ('Donald Trump');
+INSERT INTO celebrities (celebrity_name) VALUES ('Jussie Smolett');
+INSERT INTO celebrities (celebrity_name) VALUES ('Chris Brown');
+INSERT INTO celebrities (celebrity_name) VALUES ('Matt Kuh');
+
